9cd6f5958bb71ad15ed2c29f45b2d4f376b27621
Vinicius <vmayrink12@gmail.com>
2024-07-13 23:52:48 +0000

    Adding `utils.HourMinuteStringToTime` and tests

---
+++ internal/utils/util_test.go
@@ -0,0 +1,60 @@
+// vim: noexpandtab
+
+package utils_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vncsmyrnk/tiwnotify/internal/utils"
+)
+
+func TestHourMinuteStringToTime_ShouldBeOk(t *testing.T) {
+	testCases := []struct {
+		name string
+		input string
+	}{
+		{"Time1", "09:05"},
+		{"Time2", "23:59"},
+		{"Time3", "00:00"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := utils.HourMinuteStringToTime(tc.input)
+			if err != nil {
+			  t.Error("error parsing hour to time.Time", err)
+			}
+
+			now := time.Now()
+			expected, err := time.Parse(time.RFC3339, fmt.Sprintf("%4d-%02d-%02dT%v:00Z", now.Year(), int(now.Month()), now.Day(), tc.input))
+			if err != nil {
+			  t.Error("error creating expected value for test", err)
+			}
+
+			assert.Equal(t, expected, *result)
+		})
+	}
+}
+
+func TestHourMinuteStringToTime_ShouldErr(t *testing.T) {
+	testCases := []struct {
+		name string
+		input string
+	}{
+		{"Time1", "0905"},
+		{"Time2", "12:02pm"},
+		{"Time3", "two o'clock"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := utils.HourMinuteStringToTime(tc.input)
+			if err == nil {
+			  t.Error("an error should be return and it was not", err)
+			}
+		})
+	}
+}
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,7 +3,9 @@
 package utils
 
 import (
+	"fmt"
 	"reflect"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -12,4 +14,13 @@ func IgnoreFuncFields() cmp.Option {
 	return cmp.FilterValues(func(x, y interface{}) bool {
 		return reflect.TypeOf(x).Kind() == reflect.Func && reflect.TypeOf(y).Kind() == reflect.Func
 	}, cmp.Ignore())
+}
+
+func HourMinuteStringToTime(str string) (*time.Time, error) {
+	now := time.Now()
+	t, err := time.Parse(time.RFC3339, fmt.Sprintf("%4d-%02d-%02dT%v:00Z", now.Year(), int(now.Month()), now.Day(), str))
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
 }
