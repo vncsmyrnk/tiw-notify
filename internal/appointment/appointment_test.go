12400c0c4ce7304803be365d95297e0943f38f93
Vinicius Mayrink <vmayrink12@gmail.com>
2024-07-13 15:34:17 -0300

    Adding failing test on `appointment_test`. it needs fix

--- internal/appointment/appointment_test.go
+++ internal/appointment/appointment_test.go
@@ -4,11 +4,16 @@ package appointment_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/vncsmyrnk/tiwnotify/internal/appointment"
+	notificationmocks "github.com/vncsmyrnk/tiwnotify/internal/notification/mocks"
+	"github.com/vncsmyrnk/tiwnotify/internal/schedule"
+	schedulemocks "github.com/vncsmyrnk/tiwnotify/internal/schedule/mocks"
 )
 
 func TestNewAppointmentFromString_ShouldBeOk(t *testing.T) {
@@ -39,4 +44,28 @@ func TestNewAppointmentFromString_ShouldFail(t *testing.T) {
 			assert.True(t, err != nil)
 		})
 	}
+}
+
+func TestScheduleFromFile_ShouldBeOk(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	f, err := os.CreateTemp("", "sample")
+	if err != nil {
+		t.Error("failed to create temporarily file:", err)
+	}
+	f.WriteString("09:02 A random appointment\n")
+	defer os.Remove(f.Name())
+
+	mockJobScheduler := schedulemocks.NewMockJobScheduler(ctrl)
+	mockNotifier := notificationmocks.NewMockNotifier(ctrl)
+
+	now := time.Now()
+	expectedTime, _ := time.Parse(time.RFC3339, fmt.Sprintf("%4d-%02d-%02dT%v:00Z", now.Year(), int(now.Month()), now.Day(), "19:02"))
+	expectedJob, _ := schedule.NewJobByTime(expectedTime, func() {})
+	mockJobScheduler.EXPECT().AddJob(*expectedJob)
+
+	mockJobScheduler.AddJob(*expectedJob)
+	as := appointment.AppointmentSchedule{Scheduler: mockJobScheduler, Notifier: mockNotifier}
+	as.ScheduleFromFile(f.Name())
 }
